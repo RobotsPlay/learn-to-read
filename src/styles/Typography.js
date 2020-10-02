207b34a6c4cc5925fa3ea14d897cc24117727921
@johnegraham2 <johnegraham2@gmail.com>
2020-10-02 11:44:42 -0400

    removed lalezar font :(

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 import robotoLight from '../assets/fonts/roboto-light.woff2'
-import lalezar from '../assets/fonts/lalezar.woff2'
 
 const Typography = createGlobalStyle`
   @font-face {
@@ -10,14 +9,6 @@ const Typography = createGlobalStyle`
     font-weight: 300;
     font-display: swap;
     src: url(${robotoLight}) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-
-  @font-face {
-    font-family: 'Lalezar';
-    font-style: normal;
-    font-weight: 400;
-    src: url(${lalezar}) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
